228ed648ec67ee2ff2b2520caf298fe30f25ace6
Constantin Senila <senila.constantin>
2023-01-03 11:24:01 +0200

    Initial Commit

---
+++ prototypeapp/prototypeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeappApp.swift
+//  prototypeapp
+//
+//  Created by Senila Constantin on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct prototypeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
